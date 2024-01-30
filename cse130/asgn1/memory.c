959cf2cf51612d1226abda925b312d8cb5d6bf6d
Truman Vu <109135216+Trukamanzoo@users.noreply.github.com>
2024-01-30 10:49:52 +0000

    ur mom

--- cse130/asgn1/memory.c
+++ cse130/asgn1/memory.c
@@ -5,6 +5,7 @@
 #include <limits.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#define MAX_PATH 256
 
 ssize_t write_n(int fd, const void *buf, size_t n) {
     size_t total = 0;
@@ -47,11 +48,14 @@ int main() {
     ssize_t bytes_read;
     bytes_read = read(STDIN_FILENO, buffer, 4);
     printf("%s", buffer);
+    int g = 0;
     if (strcmp(buffer, "get\n") == 0){
         printf("its get\n");
+        g = 0;
         //return 0;
     } else if (strcmp(buffer, "set\n") == 0){
         printf("its set\n");
+        g = 1;
         //return 0;
     } else {
         fprintf(stderr, "urmom\n");
@@ -59,9 +63,28 @@ int main() {
     }
 
     // get file name
+    char *buffer2 = malloc((MAX_PATH + 1) * sizeof(char));
+    if (buffer2 == NULL) {
+        printf("Error: Unable to allocate memory.\n");
+        return 1;
+    }
 
-    if (bytes_read == -1) {
-        // Handle the error here. For simplicity, we just return with an error.
-        return -1;
+    int i = 0;
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        if (i >= MAX_PATH) {
+            printf("Error: Input exceeds MAX_PATH.\n");
+            free(buffer2);
+            return 1;
+        }
+        buffer2[i++] = ch;
+    }
+
+    buffer2[i] = '\0'; // Replace newline with null byte
+
+    printf("Buffer: %s\n", buffer2);
+
+    if (g){
+        int fd = open(buffer2, R_ONLY);
     }
 }
